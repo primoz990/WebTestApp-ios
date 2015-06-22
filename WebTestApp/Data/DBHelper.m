182bb23ea41bbfe737ee06a9490b25822f0122c2
primoz990 <primozr@Primozs-MacBook-Pro.local>
2015-06-22 09:39:14 +0200

    Generating SQL statements with Translated java code(j2objc)

--- WebTestApp/Data/DBHelper.m
+++ WebTestApp/Data/DBHelper.m
@@ -15,7 +15,8 @@
         //TODO in background?
         mDbQueue = [FMDatabaseQueue databaseQueueWithPath:dbPath];
         [mDbQueue inDatabase:^(FMDatabase *db) {
-            [db executeUpdate:WLDbContract_SQL_CREATE_COMPANIES_];
+            WLCompany *_c = [[WLCompany alloc] init];
+            [db executeUpdate:[_c getSqlCreateTable]];
         }];
     }
     return self;
@@ -32,38 +33,15 @@
 
 - (BOOL) setCompanies:(JavaUtilArrayList*)companies{
     [mDbQueue inDatabase:^(FMDatabase *db) {
-        BOOL b = [db executeUpdate:WLDbContract_SQL_DELETE_ALL_COMPANY_ENTRIES_];
+        WLCompany *_c = [[WLCompany alloc] init];
+        BOOL b = [db executeUpdate:[_c getSqlDeleteEntries]];
         NSLog(@"Delete all entries: %u error message: %@", b, [db lastErrorMessage]);
         for(int i=0; i<[companies size]; i++){
             WLCompany *company = [companies getWithInt:i];
-            NSString *insert = [NSString stringWithFormat:
-                    @"INSERT INTO %@ (%@, %@, %@, %@, %@, %@, %@, %@, %@, %@, %@) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                    WLDbContract_CompanyEntry_TABLE_NAME_,
-                    WLDbContract_CommonColumns_COLUMN_NAME_ID_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_TAXNUMBER_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_FULLNAME_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_SHORTNAME_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_IDNUMBER_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSPOSTNUMBER_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSHOUSENUMBER_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSMUNICIPALITY_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSPOST_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSSTREET_,
-                    WLDbContract_CompanyEntry_COLUMN_NAME_SEARCHCOLUMN_];
-            
-            b = [db executeUpdate:
-                    insert,
-                    [NSNumber numberWithInt: i],
-                    [NSNumber numberWithInt: company.getTaxNumber],
-                    company.getFullName,
-                    company.getShortName,
-                    company.getIdNumber,
-                    company.getAddressPostNumber,
-                    company.getAddressHouseNumber,
-                    company.getAddressMunicipality,
-                    company.getAddressPost,
-                    company.getAddressStreet,
-                    company.getSearchColumn];
+            NSString *insert = [company getSqlInsert];
+            if(insert && insert.length>0){
+                b = [db executeUpdate:insert];
+            }
             NSLog(@"Insert entry: %u error message: %@", b, [db lastErrorMessage]);
         }
     }];
@@ -73,21 +51,21 @@
 - (JavaUtilArrayList*) getCompanies{
     [mDbQueue inDatabase:^(FMDatabase *db) {
         JavaUtilArrayList *companies = [[JavaUtilArrayList alloc] init];
-        NSString *select = [NSString stringWithFormat:@"SELECT * FROM %@", WLDbContract_CompanyEntry_TABLE_NAME_];
-        FMResultSet *results = [db executeQuery:select];
+        WLCompany *_c = [[WLCompany alloc] init];
+        FMResultSet *results = [db executeQuery:[_c getSqlSelectAll]];
         if(results){
             while (results.next) {
                 WLCompany *company = [[WLCompany alloc] init];
-                [company setTaxNumberWithInt: [results intForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_TAXNUMBER_]];
-                [company setFullNameWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_FULLNAME_]];
-                [company setShortNameWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_SHORTNAME_]];
-                [company setIdNumberWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_IDNUMBER_]];
-                [company setAddressPostNumberWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSPOSTNUMBER_]];
-                [company setAddressHouseNumberWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSHOUSENUMBER_]];
-                [company setAddressMunicipalityWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSMUNICIPALITY_]];
-                [company setAddressPostWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSPOST_]];
-                [company setAddressStreetWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_ADDRESSSTREET_]];
-                [company setSearchColumnWithNSString: [results stringForColumn:WLDbContract_CompanyEntry_COLUMN_NAME_SEARCHCOLUMN_]];
+                [company setTaxNumberWithInt: [results intForColumn:WLCompany_SQL_COLUMN_NAME_TAXNUMBER_]];
+                [company setFullNameWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_FULLNAME_]];
+                [company setShortNameWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_SHORTNAME_]];
+                [company setIdNumberWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_IDNUMBER_]];
+                [company setAddressPostNumberWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_ADDRESSPOSTNUMBER_]];
+                [company setAddressHouseNumberWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_ADDRESSHOUSENUMBER_]];
+                [company setAddressMunicipalityWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_ADDRESSMUNICIPALITY_]];
+                [company setAddressPostWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_ADDRESSPOST_]];
+                [company setAddressStreetWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_ADDRESSSTREET_]];
+                [company setSearchColumnWithNSString: [results stringForColumn:WLCompany_SQL_COLUMN_NAME_SEARCHCOLUMN_]];
                 [companies addWithId:company];
                 NSLog(@"READ from db: %@", [company getFullName]);
             }
